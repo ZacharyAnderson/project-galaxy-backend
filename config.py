bcd796cbbfc59ef93d828fc570250667cee88453
ZacharyAnderson <zacha777@gmail.com>
2018-12-21 11:04:40 -0500

    Added secret key information

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config(object):
+    ## How this will be deployed after Dev
+    ## SECRET_KEY = os.environ.get('PROJECT-GALAXY-BACKEND-SECRET-KEY')
+    ## For now we will set the key manually for Development purposes
+    
+    SECRET_KEY = '\x82\xc5\xac-EC\xf5\x92\x11w\xe2\x18 )k\x82.\xcc\xf8 g7fA\x86~\xa7c\xd6\xfdQ\x7f\xed\xe2e21\xd5\xa5\xf6QzMg?\x85\x1e\xd5'
