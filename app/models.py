36f9226ae35b4e2ebb5d92d9983ddc6688e22886
ZacharyAnderson <zacha777@gmail.com>
2018-12-24 12:43:51 -0500

    Added isEmailUnique and isUsernameUnique routes for front end to see if email and username are unique before allowing registration to the database

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class User(db.Model):
     """User class will contain id, username, email, and password for individual users"""
     id = db.Column(db.Integer, primary_key=True)
@@ -10,7 +11,8 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+        self.password_hash = generate_password_hash(
+            password, method='pbkdf2:sha256', salt_length=8)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,30 @@
 """Routes module will have all endpoints associated with the flask application"""
-from app import app
+from app import app, db
+from app.models import User
+from flask import jsonify
+
 
 @app.route('/')
 def index():
     """index returns at the index"""
     return "Hello, Backend!"
+
+
+@app.route('/api/v1.0/registration/username/<username>', methods=['GET'])
+def isUsernameUnique(username):
+    """isUsernameUnique will return a json object if the username trying to be registered is not used."""
+    users = User.query.all()
+    for u in users:
+        if(u.username == username):
+            return jsonify({'isUnique': False})
+    return jsonify({'isUnique': True})
+
+
+@app.route('/api/v1.0/registration/email/<email>', methods=['GET'])
+def isEmailUnique(email):
+    """isEmailUnique will return a json object if the email trying to be registered is not used."""
+    users = User.query.all()
+    for u in users:
+        if(u.email == email):
+            return jsonify({'isUnique': False})
+    return jsonify({'isUnique': True})
