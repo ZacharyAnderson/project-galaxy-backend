048cbe32668d4f602dc5ed036c297108cdfb2a71
ZacharyAnderson <zacha777@gmail.com>
2018-12-24 11:38:55 -0500

    Added set password and check password functionality.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 """This module will contain all database models"""
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     """User class will contain id, username, email, and password for individual users"""
@@ -7,6 +8,12 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
