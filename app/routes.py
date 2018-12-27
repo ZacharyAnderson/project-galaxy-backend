59b9d225e4c04c705b399a05d20e9e0e191cbc6f
ZacharyAnderson <zacha777@gmail.com>
2018-12-26 22:39:20 -0500

    Added actual registration to database, will refactor tomorrow to have its own function page

--- app/routes.py
+++ app/routes.py
@@ -14,20 +14,18 @@ def index():
 @app.route('/api/v1.0/registration/username/<username>', methods=['GET'])
 def isUsernameUnique(username):
     """isUsernameUnique will return a json object if the username trying to be registered is not used."""
-    users = User.query.all()
-    for u in users:
-        if(u.username == username):
-            return jsonify({'isUnique': False})
+    user = User.query.filter_by(username=username).first()
+    if user is not None:
+        return jsonify({'isUnique': False})
     return jsonify({'isUnique': True})
 
 
 @app.route('/api/v1.0/registration/email/<email>', methods=['GET'])
 def isEmailUnique(email):
     """isEmailUnique will return a json object if the email trying to be registered is not used."""
-    users = User.query.all()
-    for u in users:
-        if(u.email == email):
-            return jsonify({'isUnique': False})
+    user = User.query.filter_by(email=email).first()
+    if user is not None:
+        return jsonify({'isUnique': False})
     return jsonify({'isUnique': True})
 
 
@@ -41,6 +39,10 @@ def registerUser():
         userEmail = json_dict['userEmail']
         userPassword = json_dict['userPassword']
 
-        print(userName + '\n' + userEmail + '\n' + userPassword)
+        user = User(username=userName, email=userEmail)
+        user.set_password(userPassword)
+        db.session.add(user)
+        db.session.commit()
 
-        return jsonify({'didItWork': True})
+        return jsonify({'registrationConfirmed': True})
+    return jsonify({'registrationConfirmed': False})
